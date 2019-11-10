7eb3b92cc030f534f7b664631aa65a0d875828a3
Ochir Darmaev <ochirdarmaev@gmail.com>
2019-11-11 00:37:06 +0300

    style: naming and refactor: loops

--- script.js
+++ script.js
@@ -1,269 +1,231 @@
 const keyboard = document.createElement('div');
-const swither = ['off', 'on'];
-const state = ['down', 'up'];
-const ClassArray = ['ctrl', 'win', 'alt', 'space', 'alt', 'arrow', 'arrow', 'arrow', 'ctrl'];
+const languages = ['ru', 'en'];
+const letterRegisters = ['low', 'upper'];
+const systemCommandKeys = ['ctrl', 'win', 'alt', 'space', 'alt', 'arrow', 'arrow', 'arrow', 'ctrl'];
 
-const keyRow1 = [
-  ['Backquote', 'ё', 'Ё', '`', '~'],
-  ['Digit1', '1', '!', '1', '!'],
-  ['Digit2', '2', '"', '2', '@'],
-  ['Digit3', '3', '№', '3', '#'],
-  ['Digit4', '4', ';', '4', '$'],
-  ['Digit5', '5', '%', '5', '%'],
-  ['Digit6', '6', ':', '6', '^'],
-  ['Digit7', '7', '?', '7', '&'],
-  ['Digit8', '8', '*', '8', '*'],
-  ['Digit9', '9', '(', '9', '('],
-  ['Digit0', '0', ')', '0', ')'],
-  ['Minus', '-', '_', '-', '_'],
-  ['Equal', '=', '+', '=', '+'],
-  ['Backspace', 'Backspace', 'Backspace', 'Backspace', 'Backspace'],
+const buttonNameIndx = 0;
+const keyRows = [
+  [
+    ['Backquote', 'ё', 'Ё', '`', '~'],
+    ['Digit1', '1', '!', '1', '!'],
+    ['Digit2', '2', '"', '2', '@'],
+    ['Digit3', '3', '№', '3', '#'],
+    ['Digit4', '4', ';', '4', '$'],
+    ['Digit5', '5', '%', '5', '%'],
+    ['Digit6', '6', ':', '6', '^'],
+    ['Digit7', '7', '?', '7', '&'],
+    ['Digit8', '8', '*', '8', '*'],
+    ['Digit9', '9', '(', '9', '('],
+    ['Digit0', '0', ')', '0', ')'],
+    ['Minus', '-', '_', '-', '_'],
+    ['Equal', '=', '+', '=', '+'],
+    ['Backspace', 'Backspace', 'Backspace', 'Backspace', 'Backspace'],
+  ],
+  [
+    ['Tab', 'Tab', 'Tab', 'Tab', 'Tab'],
+    ['KeyQ', 'й', 'Й', 'q', 'Q'],
+    ['KeyW', 'ц', 'Ц', 'w', 'W'],
+    ['KeyE', 'у', 'У', 'e', 'E'],
+    ['KeyR', 'к', 'К', 'r', 'R'],
+    ['KeyT', 'е', 'Е', 't', 'T'],
+    ['KeyY', 'н', 'Н', 'y', 'Y'],
+    ['KeyU', 'г', 'Г', 'u', 'U'],
+    ['KeyI', 'ш', 'Ш', 'i', 'I'],
+    ['KeyO', 'щ', 'Щ', 'o', 'O'],
+    ['KeyP', 'з', 'З', 'p', 'P'],
+    ['BracketLeft', 'х', 'Х', '[', '{'],
+    ['BracketRight', 'ъ', 'Ъ', ']', '}'],
+    ['Backslash', '\\', '/', '\\', '|'],
+    ['Delete', 'DEL', 'DEL', 'DEL', 'DEL'],
+  ],
+  [
+    ['CapsLock', 'CapsLock', 'CapsLock', 'CapsLock', 'CapsLock'],
+    ['KeyA', 'ф', 'Ф', 'a', 'A'],
+    ['KeyS', 'ы', 'Ы', 's', 'S'],
+    ['KeyD', 'в', 'В', 'd', 'D'],
+    ['KeyF', 'а', 'А', 'f', 'F'],
+    ['KeyG', 'п', 'П', 'g', 'G'],
+    ['KeyH', 'р', 'Р', 'h', 'H'],
+    ['KeyJ', 'о', 'О', 'j', 'J'],
+    ['KeyK', 'л', 'Л', 'k', 'K'],
+    ['KeyL', 'д', 'Д', 'l', 'L'],
+    ['Semicolon', 'ж', 'Ж', ':', ';'],
+    ['Quote', 'э', 'Э', '\'', '"'],
+    ['Enter', 'ENTER', 'ENTER', 'ENTER', 'ENTER'],
+  ],
+  [
+    ['ShiftLeft', 'Shift', 'Shift', 'Shift', 'Shift'],
+    ['KeyZ', 'я', 'Я', 'z', 'Z'],
+    ['KeyX', 'ч', 'Ч', 'x', 'X'],
+    ['KeyC', 'с', 'С', 'c', 'C'],
+    ['KeyV', 'м', 'М', 'v', 'V'],
+    ['KeyB', 'и', 'И', 'b', 'B'],
+    ['KeyN', 'т', 'Т', 'n', 'N'],
+    ['KeyM', 'ь', 'Ь', 'm', 'M'],
+    ['Comma', 'б', 'Б', ',', '<'],
+    ['Period', 'ю', 'Ю', '.', '>'],
+    ['Slash', '.', ',', '/', '?'],
+    ['ArrowUp', '▲', '▲', '▲', '▲'],
+    ['ShiftRight', 'Shift', 'Shift', 'Shift', 'Shift'],
+  ],
+  [
+    ['ControlLeft', 'Ctrl', 'Ctrl', 'Ctrl', 'Ctrl'],
+    ['MetaLeft', 'Win', 'Win', 'Win', 'Win'],
+    ['AltLeft', 'Alt', 'Alt', 'Alt', 'Alt'],
+    ['Space', ' ', ' ', ' ', ' '],
+    ['AltRight', 'Alt', 'Alt', 'Alt', 'Alt'],
+    ['ArrowLeft', '◄', '◄', '◄', '◄'],
+    ['ArrowDown', '▼', '▼', '▼', '▼'],
+    ['ArrowRight', '►', '►', '►', '►'],
+    ['ControlRight', 'Ctrl', 'Ctrl', 'Ctrl', 'Ctrl'],
+  ]
 ];
 
-const keyRow2 = [
-  ['Tab', 'Tab', 'Tab', 'Tab', 'Tab'],
-  ['KeyQ', 'й', 'Й', 'q', 'Q'],
-  ['KeyW', 'ц', 'Ц', 'w', 'W'],
-  ['KeyE', 'у', 'У', 'e', 'E'],
-  ['KeyR', 'к', 'К', 'r', 'R'],
-  ['KeyT', 'е', 'Е', 't', 'T'],
-  ['KeyY', 'н', 'Н', 'y', 'Y'],
-  ['KeyU', 'г', 'Г', 'u', 'U'],
-  ['KeyI', 'ш', 'Ш', 'i', 'I'],
-  ['KeyO', 'щ', 'Щ', 'o', 'O'],
-  ['KeyP', 'з', 'З', 'p', 'P'],
-  ['BracketLeft', 'х', 'Х', '[', '{'],
-  ['BracketRight', 'ъ', 'Ъ', ']', '}'],
-  ['Backslash', '\\', '/', '\\', '|'],
-  ['Delete', 'DEL', 'DEL', 'DEL', 'DEL'],
-];
-
-const keyRow3 = [
-  ['CapsLock', 'CapsLock', 'CapsLock', 'CapsLock', 'CapsLock'],
-  ['KeyA', 'ф', 'Ф', 'a', 'A'],
-  ['KeyS', 'ы', 'Ы', 's', 'S'],
-  ['KeyD', 'в', 'В', 'd', 'D'],
-  ['KeyF', 'а', 'А', 'f', 'F'],
-  ['KeyG', 'п', 'П', 'g', 'G'],
-  ['KeyH', 'р', 'Р', 'h', 'H'],
-  ['KeyJ', 'о', 'О', 'j', 'J'],
-  ['KeyK', 'л', 'Л', 'k', 'K'],
-  ['KeyL', 'д', 'Д', 'l', 'L'],
-  ['Semicolon', 'ж', 'Ж', ':', ';'],
-  ['Quote', 'э', 'Э', '\'', '"'],
-  ['Enter', 'ENTER', 'ENTER', 'ENTER', 'ENTER'],
-];
-
-const keyRow4 = [
-  ['ShiftLeft', 'Shift', 'Shift', 'Shift', 'Shift'],
-  ['KeyZ', 'я', 'Я', 'z', 'Z'],
-  ['KeyX', 'ч', 'Ч', 'x', 'X'],
-  ['KeyC', 'с', 'С', 'c', 'C'],
-  ['KeyV', 'м', 'М', 'v', 'V'],
-  ['KeyB', 'и', 'И', 'b', 'B'],
-  ['KeyN', 'т', 'Т', 'n', 'N'],
-  ['KeyM', 'ь', 'Ь', 'm', 'M'],
-  ['Comma', 'б', 'Б', ',', '<'],
-  ['Period', 'ю', 'Ю', '.', '>'],
-  ['Slash', '.', ',', '/', '?'],
-  ['ArrowUp', '▲', '▲', '▲', '▲'],
-  ['ShiftRight', 'Shift', 'Shift', 'Shift', 'Shift'],
-];
-
-const keyRow5 = [
-  ['ControlLeft', 'Ctrl', 'Ctrl', 'Ctrl', 'Ctrl'],
-  ['MetaLeft', 'Win', 'Win', 'Win', 'Win'],
-  ['AltLeft', 'Alt', 'Alt', 'Alt', 'Alt'],
-  ['Space', ' ', ' ', ' ', ' '],
-  ['AltRight', 'Alt', 'Alt', 'Alt', 'Alt'],
-  ['ArrowLeft', '◄', '◄', '◄', '◄'],
-  ['ArrowDown', '▼', '▼', '▼', '▼'],
-  ['ArrowRight', '►', '►', '►', '►'],
-  ['ControlRight', 'Ctrl', 'Ctrl', 'Ctrl', 'Ctrl'],
-];
-
-function CreateTextArea() {
+function createTextArea(rows, cols) {
   const TextPlace = document.createElement('textarea');
   TextPlace.id = 'result';
-  TextPlace.rows = '6';
-  TextPlace.cols = '60';
+  TextPlace.rows = rows;
+  TextPlace.cols = cols;
   document.body.append(TextPlace);
 }
 
-function CreateKeyboard() {
+function createKeyboard() {
   keyboard.className = 'keyboard';
   document.body.append(keyboard);
 }
 
-function PressCapsLock() {
+function pressCapsLock() {
   const up = document.getElementsByClassName('case');
-  for (let i = 0; i < up.length; i += 1) {
-    up[i].classList.toggle('up');
-    up[i].classList.toggle('down');
+  for (const iterator of up) {
+    iterator.classList.toggle('upper');
+    iterator.classList.toggle('low');
   }
 }
 
-function CreateButtons(firstButton, lastButton, count, array) {
-  let position = 0;
-  const CreateRow = document.createElement('div');
-  CreateRow.className = 'row';
-  keyboard.append(CreateRow);
+function createButtons(firstButton, lastButton, keyRow) {
+  const elementRow = document.createElement('div');
+  elementRow.className = 'row';
+  keyboard.append(elementRow);
+
+
+  const elementFirstButton = document.createElement('div');
   if (firstButton === '') {
-    const first = document.createElement('div');
-    first.className = 'key';
-    for (let k = 0; k < 2; k += 1) {
-      const NameButton = document.createElement('span');
-      NameButton.classList.add(array[0][0], swither[k]);
-      for (let m = 0; m < 2; m += 1) {
-        position += 1;
-        const caseName = document.createElement('span');
-        caseName.classList.add('case', state[m]);
-        caseName.innerHTML = array[0][position];
-        NameButton.append(caseName);
-      }
-      first.append(NameButton);
-    }
-    CreateRow.append(first);
+    elementFirstButton.className = 'key';
   } else {
-    const first = document.createElement('div');
-    first.classList.add('key', firstButton);
-    for (let k = 0; k < 2; k += 1) {
-      const NameButton = document.createElement('span');
-      NameButton.classList.add(array[0][0], swither[k]);
-      for (let m = 0; m < 2; m += 1) {
-        position += 1;
-        const caseName = document.createElement('span');
-        caseName.classList.add('case', state[m]);
-        caseName.innerHTML = array[0][position];
-        NameButton.append(caseName);
-      }
-      first.append(NameButton);
-    }
-    CreateRow.append(first);
+    elementFirstButton.classList.add('key', firstButton);
   }
 
-  for (let j = 0; j < count; j += 1) {
-    const CreateKey = document.createElement('div');
-    CreateKey.className = 'key';
-    position = 0;
-    for (let k = 0; k < 2; k += 1) {
-      const NameButton = document.createElement('span');
-      NameButton.classList.add(array[j + 1][0], swither[k]);
-      for (let m = 0; m < 2; m += 1) {
-        position += 1;
-        const caseName = document.createElement('span');
-        caseName.classList.add('case', state[m]);
-        caseName.innerHTML = array[j + 1][position];
-        NameButton.append(caseName);
-      }
-      CreateKey.append(NameButton);
-    }
-    CreateRow.append(CreateKey);
-  }
+  createButton(keyRow[0], elementFirstButton, elementRow);
 
-  const last = document.createElement('div');
-  last.classList.add('key', lastButton);
-  CreateRow.append(last);
-  for (let k = 0; k < 2; k += 1) {
-    const NameButton = document.createElement('span');
-    NameButton.classList.add(array[array.length - 1][0], swither[k]);
-    for (let m = 1; m < 3; m += 1) {
-      const caseName = document.createElement('span');
-      caseName.classList.add('case', state[m - 1]);
-      caseName.innerHTML = array[array.length - 1][m];
-      NameButton.append(caseName);
-    }
-    last.append(NameButton);
-  }
-}
-
-function CreateControlButtons(array, keyRow) {
-  const row = document.createElement('div');
-  row.className = 'row';
-  keyboard.append(row);
-  for (let j = 0; j < array.length; j += 1) {
+  for (let j = 1; j <= keyRow.length - 2; j++) {
     const key = document.createElement('div');
-    key.classList.add('key', array[j]);
-    for (let k = 0; k < 2; k += 1) {
-      const NameButton = document.createElement('span');
-      NameButton.classList.add(keyRow[j][0], swither[k]);
-      for (let m = 1; m < 3; m += 1) {
-        const caseName = document.createElement('span');
-        caseName.classList.add('case', state[m - 1]);
-        caseName.innerHTML = keyRow[j][m];
-        NameButton.append(caseName);
-      }
-      key.append(NameButton);
-      row.append(key);
-    }
+    key.className = 'key';
+    createButton(keyRow[j], key, elementRow);
+  }
+
+  const elementLastButton = document.createElement('div');
+  elementLastButton.classList.add('key', lastButton);
+  createButton(keyRow[keyRow.length - 1], elementLastButton, elementRow);
+}
+
+function createControlButtons(systemCommandKeys, keyRow) {
+  const rowElement = document.createElement('div');
+  rowElement.className = 'row';
+  keyboard.append(rowElement);
+  for (let j = 0; j < systemCommandKeys.length; j++) {
+    const buttonElement = document.createElement('div');
+    buttonElement.classList.add('key', systemCommandKeys[j]);
+    createButton(keyRow[j], buttonElement, rowElement);
   }
 }
 
-function ActiveLetter(key, consist) {
-  const ActiveSpan = key.getElementsByClassName('on');
-  const letter = ActiveSpan[0].getElementsByClassName(consist);
+function createButton(keyDescription, buttonElement, rowElement) {
+  let position = 1;
+  for (const language of languages) {
+    const languageElement = document.createElement('span');
+    languageElement.classList.add(keyDescription[buttonNameIndx], language);
+    for (const letterRegister of letterRegisters) {
+      const caseElement = document.createElement('span');
+      caseElement.classList.add('case', letterRegister);
+      caseElement.innerHTML = keyDescription[position++];
+      languageElement.append(caseElement);
+    }
+    buttonElement.append(languageElement);
+  }
+  rowElement.append(buttonElement);
+}
+
+function activeLetter(key, consist) {
+  const activeSpan = key.getElementsByClassName('en');
+  const letter = activeSpan[0].getElementsByClassName(consist);
 
   return letter[0];
 }
 
-CreateTextArea();
-CreateKeyboard();
-CreateButtons('', 'backspace', 12, keyRow1);
-CreateButtons('tab', 'del', 13, keyRow2);
-CreateButtons('capslock', 'enter', 11, keyRow3);
-CreateButtons('shift', 'shift-right', 11, keyRow4);
-CreateControlButtons(ClassArray, keyRow5);
+const rows = 6;
+const cols = 60;
+createTextArea(rows, cols);
+createKeyboard();
+createButtons('', 'backspace', keyRows[0]);
+createButtons('tab', 'del', keyRows[1]);
+createButtons('capslock', 'enter', keyRows[2]);
+createButtons('shift', 'shift-right', keyRows[3]);
+createControlButtons(systemCommandKeys, keyRows[4]);
 
-const TextArea = document.getElementById('result');
+const textArea = document.getElementById('result');
 const key = Array.from(document.getElementsByClassName('key'));
 
-key.forEach((element, index) => {
+key.forEach(element => {
   element.addEventListener('mousedown', () => {
-    key[index].classList.add('active');
-    if (key[index].classList[1] === 'alt' || key[index].classList[1] === 'ctrl' || key[index].classList[1] === 'win') {
-      TextArea.value += '';
-    } else if (key[index].classList[1] === 'capslock') {
-      TextArea.value += '';
-      PressCapsLock();
-    } else if (key[index].classList[1] === 'tab') {
-      TextArea.value += '  ';
-    } else if (key[index].classList[1] === 'enter') {
-      TextArea.value += '\n';
-    } else if (key[index].classList[1] === 'del') {
-      TextArea.value = '';
-    } else if (key[index].classList[1] === 'backspace') {
-      TextArea.value = TextArea.value.slice(0, -1);
-    } else if (key[index].classList[1] === 'shift') {
-      PressCapsLock();
-    } else { TextArea.value += ActiveLetter(key[index], 'down').textContent; }
+    element.classList.add('active');
+    if (element.classList[1] === 'alt' ||
+      element.classList[1] === 'ctrl' ||
+      element.classList[1] === 'win') {
+      textArea.value += '';
+    } else if (element.classList[1] === 'capslock') {
+      textArea.value += '';
+      pressCapsLock();
+    } else if (element.classList[1] === 'tab') {
+      textArea.value += '  ';
+    } else if (element.classList[1] === 'enter') {
+      textArea.value += '\n';
+    } else if (element.classList[1] === 'del') {
+      textArea.value = '';
+    } else if (element.classList[1] === 'backspace') {
+      textArea.value = textArea.value.slice(0, -1);
+    } else if (element.classList[1] === 'shift') {
+      pressCapsLock();
+    } else {
+      textArea.value += activeLetter(element, 'low').textContent;
+    }
   });
   element.addEventListener('mouseup', () => {
-    if (key[index].classList[1] === 'shift') {
-      key[index].classList.remove('active');
-      PressCapsLock();
+    if (element.classList[1] === 'shift') {
+      element.classList.remove('active');
+      pressCapsLock();
     } else {
-      key[index].classList.remove('active');
+      element.classList.remove('active');
     }
   });
 });
 
-window.addEventListener('keydown', (event) => {
-  TextArea.focus();
+window.addEventListener('keydown', event => {
+  textArea.focus();
   if (event.code) {
     const button = document.getElementsByClassName(event.code);
     button[0].parentElement.classList.add('active');
   }
 
   if (event.code === 'CapsLock') {
-    PressCapsLock();
+    pressCapsLock();
   }
 
   if (event.altKey && event.shiftKey) {
-    for (let i = 0; i < key.length; i += 1) {
-      key[i].firstChild.classList.toggle('on');
-      key[i].firstChild.classList.toggle('off');
-      key[i].lastChild.classList.toggle('off');
-      key[i].lastChild.classList.toggle('on');
+    for (let i = 0; i < key.length; i++) {
+      key[i].firstChild.classList.toggle('en');
+      key[i].firstChild.classList.toggle('ru');
+      key[i].lastChild.classList.toggle('ru');
+      key[i].lastChild.classList.toggle('en');
     }
   }
 });
